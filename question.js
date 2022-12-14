1733621dbd5e505b955bceef9378731951bbf143
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-14 09:40:31 -0800

    Update question.js

--- question.js
+++ question.js
@@ -33,6 +33,20 @@ prepand=' Midnight';
 } 
 else
 { 
+    
+//     Write a JavaScript program to compute the sum of the two given integers. If the two values are same, then returns triple their sum.
+    
+    function sumTriple (x, y) {
+  if (x == y) {
+    return 3 * (x + y);
+    } 
+   else
+   {
+    return (x + y);
+   }
+ }
+console.log(sumTriple(10, 20));
+console.log(sumTriple(10, 10));
 hour=12;
 prepand=' AM';
 } 
