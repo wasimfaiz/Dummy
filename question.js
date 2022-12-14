7e07226f9d60599e82d11aed2de5c25110816661
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-14 08:59:56 -0800

    Update question.js

--- question.js
+++ question.js
@@ -38,3 +38,31 @@ prepand=' AM';
 } 
 } 
 console.log("Current Time : "+hour + prepand + " : " + minute + " : " + second);
+
+
+// 2. Write a JavaScript program to get the current date.
+// Expected Output :
+// mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+
+var today = new Date();
+var dd = today.getDate();
+
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = mm+'-'+dd+'-'+yyyy;
+console.log(today);
+today = mm+'/'+dd+'/'+yyyy;
+console.log(today);
+today = dd+'-'+mm+'-'+yyyy;
+console.log(today);
+today = dd+'/'+mm+'/'+yyyy;
+console.log(today);
