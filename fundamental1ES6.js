8213553af775630b15a0666c054c785080fb439c
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-14 23:26:50 -0800

    Update fundamental1ES6.js

--- fundamental1ES6.js
+++ fundamental1ES6.js
@@ -9,3 +9,23 @@ const matches = (obj, source) =>
 console.log(matches({ age: 25, hair: 'long', beard: true }, { hair: 'long', beard: true })); // true
 console.log(matches({ hair: 'long', beard: true }, { age: 25, hair: 'long', beard: true })); // false
 console.log(matches({ hair: 'long', beard: true }, { age: 26, hair: 'long', beard: true })); // false
+
+
+// Write a JavaScript program to converts a comma-separated values (CSV) string to a 2D array.
+
+// Note: Use String.split('\n') to create a string for each row, then String.split(delimiter) to separate the values in each row. Omit the second argument, delimiter, to use a default delimiter of ,. Omit the third argument, omitFirstRow, to include the first row (title row) of the CSV string.
+
+// Use Array.prototype.slice() and Array.prototype.indexOf('\n') to remove the first row (title row) if omitFirstRow is true.
+// Use String.prototype.split('\n') to create a string for each row, then String.prototype.split(delimiter) to separate the values in each row.
+// Omit the second argument, delimiter, to use a default delimiter of ,.
+// Omit the third argument, omitFirstRow, to include the first row (title row) of the CSV string.
+
+const csv_to_array = (data, delimiter = ',', omitFirstRow = false) =>
+  data
+    .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
+    .split('\n')
+    .map(v => v.split(delimiter));
+
+console.log(csv_to_array('a,b\nc,d')); 
+console.log(csv_to_array('a;b\nc;d', ';')); 
+console.log(csv_to_array('head1,head2\na,b\nc,d', ',', true));
