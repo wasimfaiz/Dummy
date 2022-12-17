7b522b75208ff623a5c1f122e12fab9281f3d438
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-17 11:55:24 +0530

    Update fundamental1ES6.js

--- fundamental1ES6.js
+++ fundamental1ES6.js
@@ -74,3 +74,9 @@ const to_Safe_Integer = num =>
 console.log(to_Safe_Integer('5.2'));
 console.log(to_Safe_Integer('5.52'));
 console.log(to_Safe_Integer(Infinity));
+
+// Write a JavaScript program to find all elements in a given array except for the first one. Return the whole array if the array's length is 1.
+
+const tail = arr => (arr.length > 1 ? arr.slice(1) : arr);
+console.log(tail([1, 2, 3])); 
+console.log(tail([1]));
