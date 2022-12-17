a56249dfd228a5a236aeb9679ef1feb4e8485865
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-17 11:31:09 +0530

    Update fundamental1ES6.js

--- fundamental1ES6.js
+++ fundamental1ES6.js
@@ -66,3 +66,11 @@ const xProd = (a, b) => a.reduce((acc, x) => acc.concat(b.map(y => [x, y])), [])
 console.log(xProd([1, 2], ['a', 'b']));
 console.log(xProd([1, 2], [1, 2]));
 console.log(xProd(['a', 'b'], ['a', 'b']));
+
+// Write a JavaScript program to convert a value to a safe integer.
+
+const to_Safe_Integer = num =>
+ Math.round(Math.max(Math.min(num, Number.MAX_SAFE_INTEGER), Number.MIN_SAFE_INTEGER));
+console.log(to_Safe_Integer('5.2'));
+console.log(to_Safe_Integer('5.52'));
+console.log(to_Safe_Integer(Infinity));
