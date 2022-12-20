c25c67d70a32f1ddd96fadb51ab85dcb730e1e57
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-20 20:11:42 +0530

    Update basicprogram.js

--- basicprogram.js
+++ basicprogram.js
@@ -60,3 +60,12 @@ console.log(`The square root of ${number3} is ${result3}`);
 
 // Example 1: Area When Base and Height is Known
 
+const baseValue = prompt('Enter the base of a triangle: ');
+const heightValue = prompt('Enter the height of a triangle: ');
+
+// calculate the area
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+  `The area of the triangle is ${areaValue}`
+);
