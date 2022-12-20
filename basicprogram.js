95fc7e1b7f5a328084a9725df745ce7aa782946e
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-20 20:10:00 +0530

    Update basicprogram.js

--- basicprogram.js
+++ basicprogram.js
@@ -43,3 +43,5 @@ const number = prompt('Enter the number: ');
 
 const result = Math.sqrt(number);
 console.log(`The square root of ${number} is ${result}`);
+
+// Example 2: Square Root of Different Data Types
