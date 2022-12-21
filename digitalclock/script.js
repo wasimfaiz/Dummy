b31b3e9c3cdc8b1fb705a55f924ddff96c1eea96
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-21 12:54:37 +0530

    Create script.js

---
+++ digitalclock/script.js
@@ -0,0 +1,6 @@
+function Clock(){
+    let Time=new Date();
+    let CurTime=Time.toLocaleTimeString();
+    document.getElementById('clkId').innerHTML=CurTime;
+}
+let ChangeTime=setInterval(Clock, 1000)
