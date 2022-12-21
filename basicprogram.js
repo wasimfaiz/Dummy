dfa037bb6d9b12d449ea89b0d6d819f8e42c2b8a
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-21 19:18:07 +0530

    Update basicprogram.js

--- basicprogram.js
+++ basicprogram.js
@@ -174,3 +174,6 @@ const fahrenheit = (celsius * 1.8) + 32
 
 // display the result
 console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
+
+
+// Example 1: Using if...else
