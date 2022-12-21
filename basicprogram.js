7f2c8f50f6d6cf06aedba3306511a9c51cdd0515
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-21 19:18:28 +0530

    Update basicprogram.js

--- basicprogram.js
+++ basicprogram.js
@@ -177,3 +177,17 @@ console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenhe
 
 
 // Example 1: Using if...else
+
+// program to check if the number is even or odd
+// take input from the user
+const number = prompt("Enter a number: ");
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
