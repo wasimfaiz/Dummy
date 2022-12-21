6d1531a2af9bf1a8be6a321112b52ffdfea419ec
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-22 01:00:42 +0530

    Update question.js

--- question.js
+++ question.js
@@ -192,3 +192,15 @@ else {
 console.log("The largest number is " + largest);
 
 //Example2: Using Math.max()
+
+// program to find the largest among three numbers
+
+// take input from the user
+const num1 = parseFloat(prompt("Enter first number: "));
+const num2 = parseFloat(prompt("Enter second number: "));
+const num3 = parseFloat(prompt("Enter third number: "));
+
+const largest = Math.max(num1, num2, num3);
+
+// display the result
+console.log("The largest number is " + largest);
