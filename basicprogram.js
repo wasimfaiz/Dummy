209c17e3abaaa7b70e5b8320407241529f8d3b70
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-21 12:26:14 +0530

    Update basicprogram.js

--- basicprogram.js
+++ basicprogram.js
@@ -94,19 +94,19 @@ console.log(
 //JavaScript program to swap two variables
 
 //take input from the users
-let a = prompt('Enter the first variable: ');
-let b = prompt('Enter the second variable: ');
+let aa = prompt('Enter the first variable: ');
+let bb = prompt('Enter the second variable: ');
 
 //create a temporary variable
 let temp;
 
 //swap variables
-temp = a;
-a = b;
-b = temp;
+temp = aa;
+aa = bb;
+bb = temp;
 
-console.log(`The value of a after swapping: ${a}`);
-console.log(`The value of b after swapping: ${b}`);
+console.log(`The value of a after swapping: ${aa}`);
+console.log(`The value of b after swapping: ${bb}`);
 
 // Example: Roots of a Quadratic Equation
 
