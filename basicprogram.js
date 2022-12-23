e6dc0f30ee3b0000f30d03548428a33702ab8b74
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-23 14:51:18 +0530

    Update basicprogram.js

--- basicprogram.js
+++ basicprogram.js
@@ -263,5 +263,9 @@ for (let i = lowerNumber; i <= higherNumber; i++) {
             break;
         }
     }
-
+    
+    // if number greater than 1 and not divisible by other numbers
+    if (i > 1 && flag == 0) {
+        console.log(i);
+    }
 }
