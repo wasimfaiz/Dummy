5c8b9cd65d31f2767245462b202e845b33316928
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-24 22:06:55 +0530

    Update basicprogram.js

--- basicprogram.js
+++ basicprogram.js
@@ -40,6 +40,8 @@ console.log(`The sum of ${num1} and ${num2} is ${sum}`);
 
 // Example 1: subtract Two Numbers
 
+const num1 = 5;
+
 // Example: Square Root of a Number
 
 // take the input from the user
