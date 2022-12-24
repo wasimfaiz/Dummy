5f70ab2c3bc5f3cebf8496eea416b857d8672197
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-24 14:34:49 +0530

    Update console.js

--- console.js
+++ console.js
@@ -90,3 +90,18 @@ console.log(vowel_Count("w3resource.com"));
 
 
 //Example 1: Sum of Natural Numbers Using for Loop
+
+// program to display the sum of natural numbers
+
+// take input from the user
+const number = parseInt(prompt('Enter a positive integer: '));
+
+let sum = 0;
+
+// looping from i = 1 to number
+// in each iteration, i is increased by 1
+for (let i = 1; i <= number; i++) {
+    sum += i;
+}
+
+console.log('The sum of natural numbers:', sum);
