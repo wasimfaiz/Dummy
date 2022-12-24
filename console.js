abf801ead50a933cc4ae4159e01a643fa73f1673
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-25 01:29:36 +0530

    Update console.js

--- console.js
+++ console.js
@@ -7,6 +7,8 @@ var perimeter = (side1 + side2 + side3)/2;
 var area =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3)));
 console.log(area);
 
+//Example 
+
 const num1 = 5;
 const num2 = 3;
 
