b3d51bec5ddcdfdf08e76e4cde981b0c0ad6af03
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-26 02:10:28 +0530

    Update fundamental1ES6.js

--- fundamental1ES6.js
+++ fundamental1ES6.js
@@ -1,5 +1,6 @@
 // Write a JavaScript program to compare two objects to determine if the first one contains equivalent property 
 // values to the second one.
+// values to the second one.
 
 // Use Object.keys() to get all the keys of the second object.
 // Use Array.prototype.every(), Object.prototype.hasOwnProperty() and strict comparison to determine if all keys exist in the first object and have the same values.
