60546cc878f32564c71253dc158d99867f018951
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-27 01:49:00 +0530

    Update basicprogram.js

--- basicprogram.js
+++ basicprogram.js
@@ -282,3 +282,12 @@ for (let i = lowerNumber; i <= higherNumber; i++) {
 
 //Example: Convert Date to Number
 
+// program to convert date to number
+// create date
+const d1 = new Date();
+console.log(d1);
+
+// converting to number
+const result = d1.getTime();
+console.log(result);
+
