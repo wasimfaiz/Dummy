a94b01381b9180ea4838e36b2b1f167702f28a26
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-28 16:55:16 +0530

    Update basicprogram.js

--- basicprogram.js
+++ basicprogram.js
@@ -49,7 +49,7 @@ const sub = num1 - num2;
 // Example: Square Root of a Number
 
 // take the input from the user
-const number = prompt('Enter the number: ');
+const numberr = prompt('Enter the number: ');
 
 const result = Math.sqrt(number);
 console.log(`The square root of ${number} is ${result}`);
