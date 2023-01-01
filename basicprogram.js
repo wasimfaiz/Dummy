65ca514fc7be28f834b1f8f1318b81d6d89627ab
Wasim Faiz <thewasimfaiz@gmail.com>
2023-01-01 12:03:37 +0530

    Update basicprogram.js

--- basicprogram.js
+++ basicprogram.js
@@ -307,3 +307,22 @@ function removeItemFromArray(array, n) {
 const result = removeItemFromArray([1, 2, 3 , 4 , 5], 2);
 
 console.log(result);
+
+
+// program to loop through an object using for...in loop
+
+const student = { 
+    name: 'John',
+    age: 20,
+    hobbies: ['reading', 'games', 'coding'],
+};
+
+// using for...in
+for (let key in student) { 
+    let value;
+
+    // get the value
+    value = student[key];
+
+    console.log(key + " - " +  value); 
+} 
