ae280b614040e65859353eb7dd728df03e8608aa
Wasim Faiz <thewasimfaiz@gmail.com>
2023-01-02 20:14:53 +0530

    Update basicprogram.js

--- basicprogram.js
+++ basicprogram.js
@@ -326,3 +326,22 @@ for (let key in student) {
 
     console.log(key + " - " +  value); 
 } 
+
+
+// program to merge property of two objects
+
+// object 1
+const person = {
+    name: 'Jack',
+    age:26
+}
+
+// object 2
+const student = {
+    gender: 'male'
+}
+
+// merge two objects
+const newObj = Object.assign(person, student);
+
+console.log(newObj);
